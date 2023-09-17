ebca79f680b4cf3066e24641c0f085d08fab4d6e
h21alborzi <138998780+h21alborzi@users.noreply.github.com>
2023-09-17 22:11:47 +0330

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './features/auth/auth.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'chat-app';
+  constructor(public auth: AuthService, private router: Router) {}
+
+  signOut() {
+    this.auth.signOut().subscribe({
+      next: () => this.router.navigate(['signin'])
+    });
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  firebase: {
+    projectId: 'chat-app-tele',
+    appId: '1:927914767744:web:83eed8f5f5377a9dd9fd65',
+    storageBucket: 'chat-app-tele.appspot.com',
+    apiKey: 'AIzaSyA9mGI5hYOfgtpDTdCPz8PBdpumEWvhHBQ',
+    authDomain: 'chat-app-tele.firebaseapp.com',
+    messagingSenderId: '927914767744',
+  },
+  production: true,
+  apiUrl: 'https://us-central1-chat-app-tele.cloudfunctions.net',
+  stream: {
+    key: 'wxgsgqrws5v8'
+  }
+};
